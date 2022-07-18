523c2c9583b153103d4a3afa4311eb47e1b5ad68
Gabriela Lichy <105449193+gabrielalichy@users.noreply.github.com>
2022-07-18 14:37:30 -0300

    @Repository

--- 0x08/ProductAPI/src/main/java/com/example/product/model/ProductRepository.java
+++ 0x08/ProductAPI/src/main/java/com/example/product/model/ProductRepository.java
@@ -4,6 +4,8 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+
+@Repository
 public class ProductRepository {
 
 	private List<Product> list = new ArrayList<Product>();
